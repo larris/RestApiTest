1f32c69910c83c13e71f72857e5ceb57a110481b
lazaros fantidis <larris@lazaross-MacBook-Air.local>
2016-01-02 12:27:58 +0200

    beautified code

--- routes/bears.js
+++ routes/bears.js
@@ -3,26 +3,28 @@
  */
 var Bear = require('../models/bear');
 var express = require('express');
-  //routes for api
- 
+//routes for api
+
 var router = express.Router();
 
 router.route('/bears')
-    .post(function (req, res) {
+    .post(function(req, res) {
         //the Bear.js object
         var bear = new Bear(); // create a new instance of the Bear model
         bear.name = req.body.name; // set the bears name (comes from the request)
 
         // save the bear in mongodb and check for errors
-        bear.save(function (err) {
+        bear.save(function(err) {
             if (err)
                 res.send(err);
 
-            res.json({message: 'Bear created!'});
+            res.json({
+                message: 'Bear created!'
+            });
         });
     })
-    .get(function (req, res) { //get the bears
-        Bear.find(function (err, bears) {
+    .get(function(req, res) { //get the bears
+        Bear.find(function(err, bears) {
             if (err)
                 res.send(err);
 
@@ -32,39 +34,43 @@ router.route('/bears')
 
 
 router.route('/bears/:bear_id') //get one bear
-    .get(function (req, res) {
-        Bear.findById(req.params.bear_id, function (err, bear) {
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
             if (err)
                 res.send(err);
             res.json(bear);
         });
     })
-    .put(function (req, res) {
+    .put(function(req, res) {
         // use our bear model to find the bear we want
-        Bear.findById(req.params.bear_id, function (err, bear) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
 
             if (err)
                 res.send(err);
 
-            bear.name = req.body.name;  // update the bears info
+            bear.name = req.body.name; // update the bears info
 
             // save the bear
-            bear.save(function (err) {
+            bear.save(function(err) {
                 if (err)
                     res.send(err);
 
-                res.json({message: 'Bear updated hoooooray!'});
+                res.json({
+                    message: 'Bear updated hoooooray!'
+                });
             });
         });
     })
-    .delete(function (req, res) { //remove bear
+    .delete(function(req, res) { //remove bear
         Bear.remove({
             _id: req.params.bear_id
-        }, function (err, bear) {
+        }, function(err, bear) {
             if (err)
                 res.send(err);
 
-            res.json({message: 'Successfully deleted'});
+            res.json({
+                message: 'Successfully deleted'
+            });
         });
     });
 
--- routes/users.js
+++ routes/users.js
@@ -8,35 +8,39 @@ var router = express.Router();
 //});
 
 router.route('/users')
-    .post(function (req, res) {
+    .post(function(req, res) {
         //the User.js object
         var user = new User(); // create a new instance of the User model
         user.username = req.body.username; // set the user name (comes from the request)
         user.password = req.body.password; // set the password name (comes from the request)
-        user.email = req.body.email;// set the mail (comes from the request)
+        user.email = req.body.email; // set the mail (comes from the request)
         // save the bear in mongodb and check for errors
-        user.save(function (err) {
+        user.save(function(err) {
             if (err)
                 res.send(err);
 
-            res.json({ message: 'User created!' });
+            res.json({
+                message: 'User created!'
+            });
         });
     })
-    .get(function (req, res) { //get the user
-        User.find(function (err, users) {
+    .get(function(req, res) { //get the user
+        User.find(function(err, users) {
             if (err)
                 res.send(err);
 
             res.json(users);
         });
     });
- router.route('/users/:user_name')
-    .get(function(req,res){
-        User.find({ "username": req.params.user_name },function(err,user){
+router.route('/users/:user_name')
+    .get(function(req, res) {
+        User.find({
+            "username": req.params.user_name
+        }, function(err, user) {
             if (err)
                 res.send(err);
- 
-       res.json(user);      
+
+            res.json(user);
         });
     });
 
